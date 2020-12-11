24ad8520f0a4c058786b7aa7fb42ea953028fb0f
Ivan <56753321+khrulev@users.noreply.github.com>
2020-12-11 14:29:58 +0500

    init

---
+++ precache-manifest.8e9381c6670505a22c155559d368f548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0a12194de00f2ab047d1e5e9acafb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1db19db142824ac37b6a",
+    "url": "/static/css/main.d0b7032d.chunk.css"
+  },
+  {
+    "revision": "722540997ab86b8ef0f8",
+    "url": "/static/js/2.5683427d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5683427d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db19db142824ac37b6a",
+    "url": "/static/js/main.37d8f5cd.chunk.js"
+  },
+  {
+    "revision": "01b7661347aa06e9b55a",
+    "url": "/static/js/runtime-main.e1fc54d4.js"
+  }
+]);
